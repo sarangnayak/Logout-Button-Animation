1bb5673491dff89f736d84d8563f8bfa1ac1a342
sarangnayak <sarangnayak269@gmail.com>
2025-09-13 22:58:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  btn.classList.add("clicked");
+
+  setTimeout(() => {
+    btn.classList.add("anim1");
+  }, 300);
+
+  setTimeout(() => {
+    btn.classList.add("anim2");
+  }, 900);
+
+  setTimeout(() => {
+    btn.classList.add("logged-out");
+    btn.querySelector(".btn-text").textContent = "Logged Out";
+  }, 2500);
+});
 
